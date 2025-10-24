a755566a2d0269f76df0cb75071a639eed2d7f98
unlimon-art <unlimon@medialeadmon.com>
2025-10-24 14:08:48 -0600

    "Trigger redeploy"

--- server.js
+++ server.js
@@ -51,4 +51,4 @@ const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`🚀 Server running on port ${PORT}`);
   console.log(`📊 Loaded ${Object.keys(distribuidores).length} states`);
-});
+}); 
